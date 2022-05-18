10a88536c0a666f80b3d9e02dabc5cd378f82bc5
Olof Johansson <olof@ethup.se>
2022-05-18 14:14:02 +0200

    pack_file: Handle parsing packfiles as well
    This change copies code from srcd_pack to srcd_pack_file with the intent
    on removing it from srcd_pack in a followup commit.

--- src/srcd_pack_file.erl
+++ src/srcd_pack_file.erl
@@ -1,18 +1,69 @@
 -module(srcd_pack_file).
--export([build/2]).
+-export([read/0, build/2]).
 
+-include("srcd_object.hrl").
 -include_lib("kernel/include/logger.hrl").
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--define(MAGIC, "PACK").
--define(VERSION, 2).
+-define(state_funwrap(F), fun (State) -> F(State) end).
 
-header(Count) when Count < 4294967296 ->
-  {ok, ?MAGIC ++ [0,0,0,?VERSION] ++ binary_to_list(<<Count:32>>)};
-header(_) -> {error, too_many_objects}.
+read() ->
+  srcd_utils:pipe(#pack{}, [
+    ?state_funwrap(read_packfile_magic),
+    ?state_funwrap(read_packfile_version),
+    ?state_funwrap(read_packfile_object_count),
+    ?state_funwrap(read_packfile_objects),
+    ?state_funwrap(read_packfile_signature)
+  ]).
+
+build(Repo, Ids) ->
+  {Count, Objects} = objects(Repo, Ids),
+  ?LOG_NOTICE("Got ~p objects: ~p", [Count, Objects]),
+  case build_header(Count) of
+    {ok, Header} -> {ok, append_hash(Header ++ Objects)};
+    {error, Err} -> {error, Err}
+  end.
+
+read_packfile_magic(#pack{} = State) ->
+  case srcd_utils:read(4, crypto:hash_init(sha)) of
+    {"PACK", D} -> {ok, State#pack{hash=D}};
+    Unknown -> {error, {bad_magic, Unknown}}
+  end.
+
+read_packfile_version(#pack{hash=Digest} = State) ->
+  case srcd_utils:read_u32(Digest) of
+    {2, D} -> {ok, State#pack{version=2, hash=D}};
+    _ -> {error, unsupported_packfile_version}
+  end.
+
+read_packfile_object_count(#pack{hash=Digest} = State) ->
+  {Count, D} = srcd_utils:read_u32(Digest),
+  {ok, State#pack{count=Count, hash=D}}.
+
+read_packfile_objects(#pack{count=Count, hash=Digest} = State) ->
+  {ok, Objects, D} = read_packfile_objects(Digest, Count, []),
+  {ok, State#pack{objects=Objects, hash=D}}.
+read_packfile_objects(Digest, 0, Res) -> {ok, lists:reverse(Res), Digest};
+read_packfile_objects(Digest, Count, Res) ->
+  {ok, Object, D} = srcd_object:read(Digest),
+  ?LOG_NOTICE("Object unpacked (~p remaining): ~p", [Count-1, Object]),
+  read_packfile_objects(D, Count-1, [Object | Res]).
+
+read_packfile_signature(#pack{hash=D} = State) ->
+  Hash = crypto:hash_final(D),
+  case list_to_binary(srcd_utils:read(20)) of
+    Hash -> {ok, State#pack{hash=srcd_utils:bin_to_hex(Hash)}};
+    ShouldB -> ?LOG_NOTICE("Hash mismatch: got vs should be ~p",
+                           [{Hash, ShouldB}]),
+               {error, packfile_hash_fail}
+  end.
+
+build_header(Count) when Count >= 0 andalso Count < 4294967296 ->
+  {ok, ?PACK_MAGIC ++ [0,0,0,?PACK_VERSION] ++ binary_to_list(<<Count:32>>)};
+build_header(_) -> {error, badarg}.
 
 append_hash(Packfile) ->
   Hash = crypto:hash(sha, list_to_binary(Packfile)),
@@ -41,7 +92,7 @@ objects(Repo, Ids) -> objects(Repo, Ids, #{}, []).
 objects(_, [], _, Res) ->
   ?LOG_NOTICE("RESSS = ~p", [Res]),
   Count = length(Res),
-  Res2 = [srcd_object:encode(Obj) || Obj <- Res],
+  Res2 = [srcd_object:pack(Obj) || Obj <- Res],
   {Count, lists:flatten(lists:reverse(Res2))};
 objects(Repo, [Id | Ids], Seen, Res) ->
   case maps:is_key(Id, Seen) of
@@ -56,11 +107,4 @@ objects(Repo, [Id | Ids], Seen, Res) ->
         {error, nomatch} ->
           {error, invalid_ref}
       end
-  end.
-
-build(Repo, Ids) ->
-  {Count, Objects} = objects(Repo, Ids),
-  case header(Count) of
-    {ok, Header} -> {ok, append_hash(Header ++ Objects)};
-    {error, Err} -> {error, Err}
   end.
