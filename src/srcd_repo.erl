3640a9accfd03f6d1a8a1ad4ffeea4f0b89616f5
Olof Johansson <olof@ethup.se>
2022-05-26 14:51:44 +0200

    srcd_repo: add support to update repo

--- src/srcd_repo.erl
+++ src/srcd_repo.erl
@@ -1,11 +1,13 @@
 -module(srcd_repo).
 -behaviour(gen_server).
 -export([start_link/1, start_link/3, init/1, handle_call/3, handle_cast/2]).
--export([exists/1, exists/2, head/1, head/2, refs/1, object/2, default_branch/1]).
+-export([exists/1, exists/2, head/1, head/2, refs/1, object/2,
+         default_branch/1, write/3]).
 
 -record(?MODULE, {name, head="refs/heads/master", refs=[], objects=#{}}).
 -define(gproc_name(Repo), {via, gproc, {n, l, {?MODULE, Repo}}}).
 
+-include("srcd_object.hrl").
 -include_lib("kernel/include/logger.hrl").
 
 start_link(Repo)                -> gen_server:start_link(?gproc_name(Repo),
@@ -38,16 +40,34 @@ handle_call(refs, _, #?MODULE{head=Head, refs=Refs} = State) ->
 handle_call({object, Id}, _, #?MODULE{objects=Objs} = State) ->
   case maps:is_key(Id, Objs) of
     true -> #{Id := Obj} = Objs,
-            {reply, {ok, Obj}, State};
+            {reply, {ok, #object{id=Id, data=Obj}}, State};
     false -> {reply, {error, nomatch}, State}
   end;
 handle_call({exists, Id}, _, #?MODULE{objects=Objs} = State) ->
   {reply, maps:is_key(Id, Objs), State};
-handle_call({update, Ref, Id}, _, #?MODULE{refs=Refs} = State) ->
-  New = lists:keysort(1, lists:keymerge(1, [{Ref, Id}], Refs)),
-  {reply, ok, New}.
+handle_call({write, Cmds, #pack{objects=NewObjs}}, _,
+            #?MODULE{refs=Refs, objects=Objects} = State) ->
+  NewObjects = add_objects(Objects, NewObjs),
+  NewRefs = apply_ref_cmds(Refs, Objects, Cmds),
+  {reply, ok, State#?MODULE{refs=NewRefs, objects=NewObjects}}.
 
 handle_cast(_, State) -> {noreply, State}.
+
+apply_ref_cmds(Refs, Objects, []) -> Refs;
+apply_ref_cmds(Refs, Objects, [Cmd|Cmds]) ->
+  apply_ref_cmds(apply_ref_cmd(Refs, Objects, Cmd), Objects, Cmds).
+
+apply_ref_cmd(Refs, Objects, {update, Ref, {Old, New}}) ->
+  % TODO: verify that Ref exists, and that it points to Old before our change
+  lists:keyreplace(Ref, 1, Refs, {Ref, New});
+apply_ref_cmd(Refs, Objects, {create, Ref, New}) ->
+  lists:keysort(1, lists:keymerge(1, [{Ref, New}], Refs));
+apply_ref_cmd(Refs, _, {delete, Ref}) ->
+  lists:keydelete(Ref, 1, Refs).
+
+add_objects(Map, []) -> Map;
+add_objects(Map, [#object{id=Id, data=Obj} | Objects]) ->
+  add_objects(Map#{Id => Obj}, Objects).
 
 get_pid(Repo) -> gproc:lookup_local_name({?MODULE, Repo}).
 exists(Repo) -> is_pid(get_pid(Repo)).
@@ -63,3 +83,4 @@ head(Repo, Ref)                 -> ?call(Repo, {head, Ref}).
 refs(Repo)                      -> ?call(Repo, refs).
 object(Repo, Id)                -> ?call(Repo, {object, Id}).
 exists(Repo, Id)                -> ?call(Repo, {exists, Id}).
+write(Repo, Cmds, Packfile)     -> ?call(Repo, {write, Cmds, Packfile}).
