c343c6800572526359356bdf027dc3df0b5efcae
Olof Johansson <olof@ethup.se>
2022-06-01 17:06:20 +0200

    srcd_persist: new module for handling disk persistance
    Write git repo states to disk in various formats. This module just
    routes the persistance requests (loading/dumping) to an implementation
    module that can be selected through configuration.

---
+++ src/srcd_persist.erl
@@ -0,0 +1,23 @@
+-module(srcd_persist).
+
+% Trigger persistance (i.e write to disk), using a pluggable storage module;
+% we currently only have a sqlite implementation,.
+
+-include("srcd_object.hrl").
+-include_lib("kernel/include/logger.hrl").
+-define(forward(A), case application:get_env(srcd, persistance) of
+  {ok, PersistanceMod} -> apply(PersistanceMod, ?FUNCTION_NAME, A);
+  _ -> ok
+end).
+
+-export([init/2, load/1, dump/3, list/0]).
+
+init(Db, Name) -> ?forward([Db, Name]).
+load(Db) -> ?forward([Db]).
+dump(Db, Refs, Objects) ->
+  Res = ?forward([Db, Refs, Objects]),
+  case application:get_env(srcd, persistance_copy) of
+    {ok, Mod} -> Mod:dump(Db, Refs, Objects);
+    _ -> Res
+  end.
+list() -> ?forward([]).
