ec89962a16d23f1dd446638e32f5297f1248a879
Olof Johansson <olof@ethup.se>
2022-06-03 14:36:47 +0200

    Merge srcd_app with srcd module

--- src/srcd.erl
+++ src/srcd.erl
@@ -1,8 +1,16 @@
+%%%-------------------------------------------------------------------
+%% @doc srcd public API
+%% @end
+%%%-------------------------------------------------------------------
+
 -module(srcd).
+-behaviour(application).
+
+-export([start/2, stop/1]).
 -export([new/1, info/1]).
 
-new(Name) ->
-  ok = srcd_repo:create(Name).
+start(_StartType, _StartArgs) -> srcd_sup:start_link().
+stop(_State) -> ok.
 
-info(Name) ->
-  srcd_repo:info(Name).
+new(Name) -> ok = srcd_repo:create(Name).
+info(Name) -> srcd_repo:info(Name).
