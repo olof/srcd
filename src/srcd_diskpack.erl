1a79d7a7d57ece93785f1ecd8866a4501ed25089
Olof Johansson <olof@ethup.se>
2022-06-03 14:53:48 +0200

    srcd_diskpack: refactor file:open calls

--- src/srcd_diskpack.erl
+++ src/srcd_diskpack.erl
@@ -13,14 +13,14 @@ init(Packfile, Name) ->
 
   ok = write_meta(Packfile, Name),
 
-  {ok, Fh} = file:open(Packfile ++ ".pack", [write]),
+  {ok, Fh} = open(pack, Packfile, [write]),
   {ok, EmptyPack} = srcd_packfile:build(Name, []),
   ok = io:put_chars(Fh, EmptyPack),
   ok = file:close(Fh),
 
-  {ok, FhRefs} = file:open(Packfile ++ ".pack.refs", [write]),
+  {ok, FhRefs} = open(refs, Packfile, [write]),
   % Good job!
-  ok = file:close(Fh),
+  ok = file:close(FhRefs),
 
   ok.
 
@@ -47,19 +47,24 @@ list() ->
    Pack <- Files,
    filename:extension(Pack) =:= ".pack"]}.
 
+open(Type, Packfile)       -> open(Type, Packfile, [read]).
+open(pack, Packfile, Opts) -> file:open(Packfile ++ ".pack", Opts);
+open(refs, Packfile, Opts) -> file:open(Packfile ++ ".pack.refs", Opts);
+open(meta, Packfile, Opts) -> file:open(Packfile ++ ".pack.meta", Opts).
+
 read_meta(Packfile) ->
-  {ok, Fh} = file:open(Packfile ++ ".pack.meta", [read]),
+  {ok, Fh} = open(meta, Packfile),
   Res = [{name, string:trim(io:get_line(Fh, ""))}],
   ok = file:close(Fh),
   {ok, Res}.
 
 write_meta(Packfile, Name) ->
-  {ok, Fh} = file:open(Packfile ++ ".pack.meta", [write]),
+  {ok, Fh} = open(meta, Packfile, [write]),
   ok = io:put_chars(Fh, Name),
   ok = file:close(Fh).
 
 read_refs(Packfile) ->
-  {ok, Fh} = file:open(Packfile ++ ".pack.refs", [read]),
+  {ok, Fh} = open(refs, Packfile),
   Refs = lists:keysort(1, read_refs(Fh, [])),
   ok = file:close(Fh),
   {ok, Refs}.
@@ -74,7 +79,7 @@ read_refs(Fh, Refs) ->
 write_refs(Packfile, Cmds) when is_list(Packfile) ->
   {ok, Old} = read_refs(Packfile),
   {ok, New} = update_refs(maps:from_list(Old), Cmds),
-  {ok, Fh} = file:open(Packfile ++ ".pack.refs", [write]),
+  {ok, Fh} = open(refs, Packfile, [write]),
   write_refs(Fh, lists:keysort(1, maps:to_list(New)));
 write_refs(Fh, []) -> file:close(Fh);
 write_refs(Fh, [{Ref, Id}|New]) ->
@@ -101,14 +106,12 @@ apply_ref_cmd(Old, {delete, Ref, Id}) ->
   end.
 
 append_objects(Packfile, Objects) when is_list(Packfile) ->
-  P = Packfile ++ ".pack",
-
-  {ok, Fh1} = file:open(P, [read, binary]),
+  {ok, Fh1} = open(pack, Packfile, [read, binary]),
   <<"PACK", 2:32, OldCount:32>> = io:get_chars(Fh1, "", 12),
   NewCount = OldCount + length(Objects),
   ok = file:close(Fh1),
 
-  {ok, Fh2} = file:open(P, [write, read]),
+  {ok, Fh2} = open(pack, Packfile, [write, read]),
   {ok, Pos} = file:position(Fh2, {eof, -20}),
   ok = file:truncate(Fh2),
   ok = write_objects(Fh2, Objects),
@@ -116,11 +119,11 @@ append_objects(Packfile, Objects) when is_list(Packfile) ->
   file:write(Fh2, <<"PACK", 2:32, NewCount:32>>),
   ok = file:close(Fh2),
 
-  {ok, Fh3} = file:open(Packfile ++ ".pack", [read, binary]),
+  {ok, Fh3} = open(pack, Packfile, [read, binary]),
   {ok, Digest} = hash_packfile(Fh3),
   ok = file:close(Fh3),
 
-  {ok, Fh4} = file:open(Packfile ++ ".pack", [write, append, binary]),
+  {ok, Fh4} = open(pack, Packfile, [write, append, binary]),
   ok = file:write(Fh4, Digest),
   ok = file:close(Fh4),
 
@@ -146,7 +149,7 @@ hash_packfile(Fh, D) ->
   end.
 
 read_objects(Packfile) ->
-  {ok, Fh} = file:open(Packfile ++ ".pack", [read]),
+  {ok, Fh} = open(pack, Packfile),
   {ok, #pack{objects=Objects}} = srcd_packfile:read(Fh),
   ok = file:close(Fh),
   ?LOG_NOTICE("loaded objects: ~p", [Objects]),
