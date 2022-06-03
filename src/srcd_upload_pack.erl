2b1ae48c9060e2c4acb8fb0790331da0459c8a2d
Olof Johansson <olof@ethup.se>
2022-06-03 15:15:16 +0200

    learn you some british english for great good

--- src/srcd_upload_pack.erl
+++ src/srcd_upload_pack.erl
@@ -1,7 +1,7 @@
 % ex:ts=2:sw=2:sts=2:et
 % -*- tab-width: 2; c-basic-offset: 2; indent-tabs-mode: nil -*-
 -module(srcd_upload_pack).
--behavior(srcd_cmd).
+-behaviour(srcd_cmd).
 -export([init/2, caps/0]).
 -export([
   advertise/1,
