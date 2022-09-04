ef49e56be0f59f8139bbe983c22737504b444737
Olof Johansson <olof@ethup.se>
2022-09-05 00:18:31 +0200

    flate: add support for (length, distance) pairs
    When a length code is encountered, the next symbol comes from the
    distance alphabet, and it tells us how far back we should start copying
    bytes from. Length tells us the number of bytes, naturally.

--- apps/flate/src/flate.erl
+++ apps/flate/src/flate.erl
@@ -129,8 +129,10 @@ inflate_symbols(Huffman, Data, Symbols, Bits) ->
 	   (Bits + Len) div 8 + case Bits + Len rem 8 of 0 -> 0; _ -> 1 end};
     {ok, {Len, _, Symbol}, Tail} when Symbol < 256 ->
       inflate_symbols(Huffman, Tail, [Symbol | Symbols], Bits + Len);
-    {ok, {Len, Code, Symbol}, _} ->
-      {error, not_implemented, repetitions, {Len, Code, Symbol}}
+    {ok, {_, _, Code}, Tail1} ->
+      {Length, Dist, Tail, Read} = decode_distance_pair(Huffman, Code, Tail1),
+      inflate_symbols(Huffman, Tail, [clone_output(lists:flatten(Symbols), Dist, Length) | Symbols],
+                      Bits + Read)
   end.
 
 -ifdef(TEST).
@@ -150,6 +152,91 @@ inflate_symbols_test_() -> lists:concat([
 
 -endif.
 
+clone_output(Symbols, Dist, Length) ->
+  {_, Buf} = lists:split(length(Symbols)-Dist, Symbols),
+  clone_output(Buf, Length, [], []).
+clone_output(_, 0, Cur, Acc) ->
+  lists:concat(lists:reverse([lists:reverse(Cur)|Acc]));
+clone_output([], Len, Cur, Acc) ->
+  Buf = lists:concat(lists:reverse([lists:reverse(Cur)|Acc])),
+  clone_output(Buf, Len, [], [Buf]);
+clone_output([], N, _, _) ->
+  {error, distance_too_far_back, N};
+clone_output([S|Buf], Length, Cur, Acc) ->
+  clone_output(Buf, Length-1, [S|Cur], Acc).
+
+-ifdef(TEST).
+
+clone_output_test_() -> [
+  ?_assertEqual("d",    clone_output("abcd", 1, 1)),
+  ?_assertEqual("dd",   clone_output("abcd", 1, 2)),
+  ?_assertEqual("dddd", clone_output("abcd", 1, 4)),
+
+  ?_assertEqual("c",    clone_output("abcd", 2, 1)),
+  ?_assertEqual("cd",   clone_output("abcd", 2, 2)),
+  ?_assertEqual("cdcd", clone_output("abcd", 2, 4)),
+
+  ?_assertEqual("abcd", clone_output("abcd", 4, 4)),
+  ?_assertEqual("abcda", clone_output("abcd", 4, 5)),
+  ?_assertEqual("abcdabcd", clone_output("abcd", 4, 8)),
+  ?_assertEqual("abcdabcda", clone_output("abcd", 4, 9))
+].
+
+-endif.
+
+decode_distance_pair(Huffman, Code, Data) ->
+  %% We got our length, now we only need our distance:
+  % we should be able to construct a code table for the distances,
+  % and pass it instead of Huffman as the first parameter:
+  %{ok, {Read, _, Sym}, Tail} = flate_huffman:get_symbol(Huffman, Data),
+
+  {Length, LengthTail, BitsRead1} = decode_distance_len(Code, Data),
+  {Distance, DistanceTail, BitsRead2} = decode_distance(Code, LengthTail),
+
+  % TODO: Don't forget to do accounting on ExtraBits!
+  {Length, Distance, DistanceTail, BitsRead1 + BitsRead2}.
+
+decode_distance_len(Code, Data) ->
+  {ExtraBits, Len} = distance_code_length(Code),
+  {Extra, Tail} = read_bits(Data, ExtraBits),
+  {(Len bsl ExtraBits) + Extra, Tail, ExtraBits}.
+
+decode_distance(Code, Data) ->
+  {DistanceCode, Extras} = read_bits(Data, 5),
+  {DistanceCode, ExtraBits} = distance_extra_bits(DistanceCode),
+  {Extra, Tail} = read_bits(Extras, ExtraBits),
+  {(DistanceCode bsl ExtraBits) + Extra + 1, Tail, 5 + ExtraBits}.
+
+read_bits(Data, Count) ->
+  case {Count, Data} of
+    {0, _} -> {0, Data};
+
+    {Count, Data} when is_binary(Data) andalso Count rem 8 == 0 ->
+      <<Bits:Count, Tail/binary>> = Data,
+      {Bits, Tail};
+
+    {Count, Data} when is_binary(Data) andalso Count rem 8 > 0 ->
+      Pad = 8 - Count rem 8,
+      <<Bits:Count, TailBits:Pad/bits, Tail/binary>> = Data,
+      {Bits, {TailBits, Tail}};
+
+    {Count, {Bits, Bin}} when bit_size(Bits) == Count ->
+      <<Int:Count>> = Bits,
+      {Int, Bin};
+
+    {Count, {Bits1, Bin}} when bit_size(Bits1) > Count ->
+      <<Bits:Count, BitsTail/bits>> = Bits1,
+      {Bits, {BitsTail, Bin}};
+
+    {Count, {Bits1, Bin}} when bit_size(Bits1) < Count ->
+      ?LOG_NOTICE("Count: ~p, Bits: ~p, Bin: ~p", [Count, Bits1, Bin]),
+      PadSize = 8 - Count rem 8,
+      <<Byte:8/bits, Tail/binary>> = Bin,
+      <<Bits:Count, BitsTail/bits>> = <<Bits1/bits, Byte/bits>>,
+      {Bits, {BitsTail, Tail}}
+  end.
+
+
 fixed() ->
   % Literal value    Bits                 Codes
   % -------------------------------------------------------
@@ -163,6 +250,39 @@ fixed() ->
     [{X, 7} || X <- lists:seq(256, 279)],
     [{X, 8} || X <- lists:seq(280, 287)]
   ]).
+
+distance_extra_bits() ->
+  % Code table for distance alphabet from RFC 1951 section 3.2.5
+  lists:concat([
+    [{X,  0} || X <- [ 0,  1, 2, 3]],
+    [{X,  1} || X <- [ 4,  5]],
+    [{X,  2} || X <- [ 6,  7]],
+    [{X,  3} || X <- [ 8,  9]],
+    [{X,  4} || X <- [10, 11]],
+    [{X,  5} || X <- [12, 13]],
+    [{X,  6} || X <- [14, 15]],
+    [{X,  7} || X <- [16, 17]],
+    [{X,  8} || X <- [18, 19]],
+    [{X,  9} || X <- [20, 21]],
+    [{X, 10} || X <- [22, 23]],
+    [{X, 11} || X <- [24, 25]],
+    [{X, 12} || X <- [26, 27]],
+    [{X, 13} || X <- [28, 29]]
+  ]).
+distance_extra_bits(Code) ->
+  lists:keyfind(Code, 1, distance_extra_bits()).
+
+distance_code_length(Code) ->
+  case Code of
+    257 -> {0,   3}; 258 -> {0,   4}; 259 -> {0,   5}; 260 -> {0,   6};
+    261 -> {0,   7}; 262 -> {0,   8}; 263 -> {0,   9}; 264 -> {0,  10};
+    265 -> {1,  11}; 266 -> {1,  13}; 267 -> {1,  15}; 268 -> {1,  17};
+    269 -> {2,  19}; 270 -> {2,  23}; 271 -> {2,  27}; 272 -> {2,  31};
+    273 -> {3,  35}; 274 -> {3,  43}; 275 -> {3,  51}; 276 -> {3,  59};
+    277 -> {4,  67}; 278 -> {4,  83}; 279 -> {4,  99}; 280 -> {4, 115};
+    281 -> {5, 131}; 282 -> {5, 163}; 283 -> {5, 195}; 284 -> {5, 227};
+    285 -> {0, 258}
+  end.
 
 int_to_btype(0) -> no_compression;
 int_to_btype(1) -> huffman_fixed;
