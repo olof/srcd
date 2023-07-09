1c58d2044a0af5977cc8386f4561851795f59538
Olof Johansson <olof@ethup.se>
2023-07-10 01:42:43 +0200

    pack: Implement line splitting, respecting max line length

--- src/srcd_pack.erl
+++ src/srcd_pack.erl
@@ -1,15 +1,39 @@
 % ex:ts=2:sw=2:sts=2:et
 % -*- tab-width: 2; c-basic-offset: 2; indent-tabs-mode: nil -*-
 -module(srcd_pack).
--export([advertisement/3, build_pkt/1, read_line/0, read_command/0]).
+-export([
+  advertisement/3,
+  build_pkt/1,
+  line_split/1,
+  line_split/2,
+  max_data_len/0,
+  read_line/0,
+  read_command/0
+]).
 -export([reflines/1, reflines_with_head/2]).
 
 -include_lib("kernel/include/logger.hrl").
 
+-define(MAX_PACKET_DATA_LEN, 65516).
+
+-ifdef(TEST).
+-include("tests/pack.trl").
+-endif.
+
+max_data_len() -> ?MAX_PACKET_DATA_LEN.
+
+line_split(Str) -> line_split(?MAX_PACKET_DATA_LEN, Str).
+line_split(Len, Str) -> line_split(Len, Str, []).
+line_split(Len, [], Acc) -> lists:reverse(Acc);
+line_split(Len, Str, Acc) when length(Str) < Len -> lists:reverse([Str | Acc]);
+line_split(Len, Str, Acc) ->
+  {Head, Tail} = lists:split(Len, Str),
+  line_split(Len, Tail, [Head | Acc]).
+
 pkt_line(flush) -> "0000";
 pkt_line(delim) -> "0001";
-pkt_line(Line) ->
-  lists:concat([io_lib:format("~4.16.0b", [length(Line) + 4]), Line]).
+pkt_line(Line) when length(Line) =< ?MAX_PACKET_DATA_LEN ->
+  [io_lib:format("~4.16.0b", [length(Line) + 4]), Line].
 
 no_such_repo(Repo) ->
   ?LOG_NOTICE("requested repo ~p does not exist", [Repo]),
@@ -40,7 +64,7 @@ prepend_version(1, Pkt) -> ["version 1\n" | Pkt];
 prepend_version(2, Pkt) -> ["version 2\n" | Pkt].
 
 build_pkt(Lines) ->
-  {ok, lists:concat([pkt_line(Line) || Line <- Lines])}.
+  {ok, lists:flatten([pkt_line(Line) || Line <- Lines])}.
 
 capstring(Caps) -> string:join([capability(C) || C <- Caps], " ").
 capability({Key, Value}) when is_atom(Key) ->
