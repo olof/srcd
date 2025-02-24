999d2fe0c0372b01d728d3360116d023884a7b01
Olof Johansson <olof@ethup.se>
2025-02-24 22:54:34 +0100

    flate: Refactor out lz77 code

--- apps/flate/src/flate.erl
+++ apps/flate/src/flate.erl
@@ -206,14 +206,12 @@ inflate_symbols(Huffman, Data, Symbols, BitCount) ->
       inflate_symbols(Huffman, Tail, [Symbol | Symbols], BitCount + Len);
 
     {ok, {Len, _, Code}, Tail1} ->
-      case decode_distance_pair(Huffman, Code, Tail1) of
+      case flate_lz77:decode(Symbols, Code, Tail1) of
         {more, N} -> {more, N};
-        {Length, Dist, Tail, Read} ->
-          case clone_output(lists:flatten(Symbols), Dist, Length) of
-            {error, Reason} -> error({error, Reason});
-            Output -> inflate_symbols(Huffman, Tail, [Output | Symbols],
-                                      BitCount + Len + Read)
-          end
+        {error, Reason} -> error({error, Reason});
+        {ok, Output, Tail, Read} ->
+          inflate_symbols(Huffman, Tail, [Output | Symbols],
+                          BitCount + Len + Read)
       end
   end.
 
@@ -233,78 +231,6 @@ inflate_symbols_test_() -> lists:concat([
 ]).
 
 -endif.
-
-clone_output(Symbols, Dist, _) when Dist > length(Symbols) ->
-  {error, {deflate_distance_too_far_back, Dist, length(Symbols)}};
-clone_output(Symbols, Dist, Length) ->
-  {_, Buf} = lists:split(length(Symbols)-Dist, Symbols),
-  clone_output(Buf, Length, [], []).
-
-clone_output(_, 0, Cur, Acc) ->
-  lists:concat(lists:reverse([lists:reverse(Cur)|Acc]));
-clone_output([], Len, Cur, Acc) ->
-  Buf = lists:concat(lists:reverse([lists:reverse(Cur)|Acc])),
-  clone_output(Buf, Len, [], [Buf]);
-clone_output([S|Buf], Length, Cur, Acc) ->
-  clone_output(Buf, Length-1, [S|Cur], Acc).
-
--ifdef(TEST).
-
-clone_output_test_() -> [
-  ?_assertEqual("d",    clone_output("abcd", 1, 1)),
-  ?_assertEqual("dd",   clone_output("abcd", 1, 2)),
-  ?_assertEqual("dddd", clone_output("abcd", 1, 4)),
-
-  ?_assertEqual("c",    clone_output("abcd", 2, 1)),
-  ?_assertEqual("cd",   clone_output("abcd", 2, 2)),
-  ?_assertEqual("cdcd", clone_output("abcd", 2, 4)),
-
-  ?_assertEqual("abcd", clone_output("abcd", 4, 4)),
-  ?_assertEqual("abcda", clone_output("abcd", 4, 5)),
-  ?_assertEqual("abcdabcd", clone_output("abcd", 4, 8)),
-  ?_assertEqual("abcdabcda", clone_output("abcd", 4, 9)),
-
-  ?_assertEqual(lists:duplicate(258, $?), clone_output("?", 1, 258)),
-  ?_assertEqual(lists:flatten(lists:duplicate(129, "c?")),
-                clone_output("abc?", 2, 258))
-].
-
--endif.
-
-decode_distance_pair(Huffman, Code, Data) ->
-  case decode_distance_len(Code, Data) of
-    {more, N} -> {more, N};
-    {Length, LengthTail, BitsRead1} ->
-      case decode_distance(LengthTail) of
-        {more, N} -> {more, N};
-        {Distance, DistanceTail, BitsRead2} ->
-          {Length, Distance, DistanceTail, BitsRead1 + BitsRead2}
-      end
-  end.
-
-decode_distance_len(Code, Data) ->
-  {ExtraBits, Len} = distance_code_length(Code),
-  case read_bits(Data, ExtraBits) of
-    % TODO: improve {more, 1} to calculate how many bytes we should read
-    {error, insufficient_data} -> {more, 1};
-    %                 {((Len bsl ExtraBits) + Extra, Tail, ExtraBits}.
-    {Extra, Tail} -> {Len + flate_utils:reverse_int(Extra, ExtraBits),
-                      Tail, ExtraBits}
-  end.
-
-decode_distance(Data) ->
-  case read_bits(Data, 5, []) of
-    {error, insufficient_data} -> {more, 1};
-    {RevCode, Extras} ->
-      Code = flate_utils:reverse_int(RevCode, 5),
-      Base = distance_base(Code),
-      {Code, ExtraBits} = distance_extra_bits(Code),
-      case read_bits(Extras, ExtraBits) of
-        {error, insufficient_data} -> {more, 1};
-        {Extra, Tail} ->
-          {Base + flate_utils:b2i(Extra) + 1, Tail, 5 + ExtraBits}
-      end
-  end.
 
 dynamic(Data) ->
   % Literal value    Bits                 Codes
@@ -353,112 +279,6 @@ fixed() ->
     [{X, 7} || X <- lists:seq(256, 279)],
     [{X, 8} || X <- lists:seq(280, 287)]
   ]).
-
--define(distance_code(Lower, Upper, Extra),
-        distance_code(N) when N >= Lower andalso N =< Upper div 4 ->
-	  {N, {5, Extra * 2 + 3}};
-        distance_code(N) when N > Upper div 4 andalso (N =< Upper) ->
-	  {N, {5, Extra * 2 + 3}}).
-
-distance_codes() -> [distance_code(N) || N <- lists:seq(1, 32768) ].
-distance_code(1) -> {1, {5, 0}};
-distance_code(2) -> {2, {5, 1}};
-distance_code(3) -> {3, {5, 2}};
-distance_code(4) -> {4, {5, 3}};
-?distance_code(      5,     8,   1);
-?distance_code(      9,    16,   2);
-?distance_code(     17,    32,   3);
-?distance_code(     33,    64,   4);
-?distance_code(     65,   128,   5);
-?distance_code(    129,   256,   6);
-?distance_code(    257,   512,   7);
-?distance_code(    513,  1024,   8);
-?distance_code(   1025,  2048,   9);
-?distance_code(   2049,  4096,  10);
-?distance_code(   4097,  8192,  11);
-?distance_code(   8193, 16384,  12);
-?distance_code(  16385, 32768,  13).
-
-distance_base() -> [
-  { 0,     1},
-  { 1,     2},
-  { 2,     3},
-  { 3,     4},
-  { 4,     5},
-  { 5,     7},
-  { 6,     9},
-  { 7,    13},
-  { 8,    17},
-  { 9,    25},
-  {10,    33},
-  {11,    49},
-  {12,    65},
-  {13,    97},
-  {14,   129},
-  {15,   193},
-  {16,   257},
-  {17,   385},
-  {18,   513},
-  {19,   769},
-  {20,  1025},
-  {21,  1537},
-  {22,  2049},
-  {23,  3073},
-  {24,  4097},
-  {25,  6145},
-  {26,  8193},
-  {27, 12289},
-  {28, 16385},
-  {29, 24577}
-].
-distance_base(Code) ->
-  {Code, Value} = lists:keyfind(Code, 1, distance_base()),
-  Value.
-
-distance_length_base() -> [
-  { 0,   3}, { 1,   4}, { 2,   5}, { 3,   6}, { 4,   7}, { 5,   8}, { 6,   9},
-  { 7,  10}, { 8,  11}, { 9,  13}, {10,  15}, {11,  17}, {12,  19}, {13,  23},
-  {14,  27}, {15,  31}, {16,  35}, {17,  43}, {18,  51}, {19,  59}, {20,  67},
-  {21,  83}, {22,  99}, {23, 115}, {24, 131}, {25, 163}, {26, 195}, {27, 227},
-  {28, 258}
-].
-distance_length_base(Code) ->
-  {Code, Value} = lists:keyfind(Code, 1, distance_length_base()),
-  Value.
-
-distance_extra_bits() ->
-  % Code table for distance alphabet from RFC 1951 section 3.2.5
-  lists:concat([
-    [{X,  0} || X <- [ 0,  1, 2, 3]],
-    [{X,  1} || X <- [ 4,  5]],
-    [{X,  2} || X <- [ 6,  7]],
-    [{X,  3} || X <- [ 8,  9]],
-    [{X,  4} || X <- [10, 11]],
-    [{X,  5} || X <- [12, 13]],
-    [{X,  6} || X <- [14, 15]],
-    [{X,  7} || X <- [16, 17]],
-    [{X,  8} || X <- [18, 19]],
-    [{X,  9} || X <- [20, 21]],
-    [{X, 10} || X <- [22, 23]],
-    [{X, 11} || X <- [24, 25]],
-    [{X, 12} || X <- [26, 27]],
-    [{X, 13} || X <- [28, 29]]
-  ]).
-distance_extra_bits(Code) ->
-  lists:keyfind(Code, 1, distance_extra_bits()).
-
-distance_code_length(Code) ->
-  {L, _} = case Code of
-    257 -> {0,   3}; 258 -> {0,   4}; 259 -> {0,   5}; 260 -> {0,   6};
-    261 -> {0,   7}; 262 -> {0,   8}; 263 -> {0,   9}; 264 -> {0,  10};
-    265 -> {1,  11}; 266 -> {1,  13}; 267 -> {1,  15}; 268 -> {1,  17};
-    269 -> {2,  19}; 270 -> {2,  23}; 271 -> {2,  27}; 272 -> {2,  31};
-    273 -> {3,  35}; 274 -> {3,  43}; 275 -> {3,  51}; 276 -> {3,  59};
-    277 -> {4,  67}; 278 -> {4,  83}; 279 -> {4,  99}; 280 -> {4, 115};
-    281 -> {5, 131}; 282 -> {5, 163}; 283 -> {5, 195}; 284 -> {5, 227};
-    285 -> {0, 258}
-  end.
-  %{L, Code}.
 
 int_to_btype(0) -> no_compression;
 int_to_btype(1) -> huffman_fixed;
@@ -545,28 +365,28 @@ test_inflate_steps() ->
   ?assertEqual({<<0:5>>, Tail4 = <<163, 96, 20, 140, 2, 8, 0, 0>>}, Tail3),
   % head of the bin list is: 2#00000101
   % we steal the bits 101 (use them as lsb of code 197), and left is 00000.
-  {258, 2, Tail4, 5} = decode_distance_pair(Fixed, 285, Tail3),
-  MaxSubstr = clone_output([63, 63], 2, 258),
+  {258, 2, Tail4, 5} = flate_lz77:decode_distance_pair(285, Tail3),
+  MaxSubstr = flate_lz77:clone_output([63, 63], 2, 258),
 
   % Repeat 2
   {ok, {8, 197, 285}, Tail5 = {<<>>, <<96, 20, 140, 2, 8, 0, 0>>}} = flate_huffman:get_symbol(Fixed, Tail4),
-  {258, 2, Tail6 = {<<6:3>>, <<20, 140, 2, 8, 0, 0>>}, 5} = decode_distance_pair(Fixed, 285, Tail5),
-  MaxSubstr = clone_output([63, 63] ++ MaxSubstr, 2, 258),
+  {258, 2, Tail6 = {<<6:3>>, <<20, 140, 2, 8, 0, 0>>}, 5} = flate_lz77:decode_distance_pair(285, Tail5),
+  MaxSubstr = flate_lz77:clone_output([63, 63] ++ MaxSubstr, 2, 258),
 
   % Repeat 3
   {ok, {8, 197, 285}, Tail7} = flate_huffman:get_symbol(Fixed, Tail6),
-  {258, 2, Tail8, 5} = decode_distance_pair(Fixed, 285, Tail7),
-  MaxSubstr = clone_output([63, 63] ++ MaxSubstr ++ MaxSubstr, 2, 258),
+  {258, 2, Tail8, 5} = flate_lz77:decode_distance_pair(285, Tail7),
+  MaxSubstr = flate_lz77:clone_output([63, 63] ++ MaxSubstr ++ MaxSubstr, 2, 258),
 
   % Repeat 4
   {ok, {8, 197, 285}, Tail9} = flate_huffman:get_symbol(Fixed, Tail8),
-  {258, 2, Tail10, 5} = decode_distance_pair(Fixed, 285, Tail9),
-  MaxSubstr = clone_output([63, 63] ++ MaxSubstr ++ MaxSubstr ++ MaxSubstr, 2, 258),
+  {258, 2, Tail10, 5} = flate_lz77:decode_distance_pair(285, Tail9),
+  MaxSubstr = flate_lz77:clone_output([63, 63] ++ MaxSubstr ++ MaxSubstr ++ MaxSubstr, 2, 258),
 
   % Repeat 5, shorter length
   {ok, {7, 4, 260}, Tail11} = flate_huffman:get_symbol(Fixed, Tail10),
-  {6, 2, Tail12, 5} = decode_distance_pair(Fixed, 260, Tail11),
-  ShortSubstr = clone_output([63, 63] ++ MaxSubstr ++ MaxSubstr ++ MaxSubstr ++ MaxSubstr, 2, 6),
+  {6, 2, Tail12, 5} = flate_lz77:decode_distance_pair(260, Tail11),
+  ShortSubstr = flate_lz77:clone_output([63, 63] ++ MaxSubstr ++ MaxSubstr ++ MaxSubstr ++ MaxSubstr, 2, 6),
 
   % end marker (7 bits) and padding (6 bits)
   ?assertEqual({<<0:5>>, <<0>>}, Tail12).
---
+++ apps/flate/src/flate_lz77.erl
@@ -0,0 +1,198 @@
+% ex:ts=2:sw=2:sts=2:et:foldmarker=-ifdef(TEST).,-endif.:foldmethod=marker
+% -*- tab-width: 2; c-basic-offset: 2; indent-tabs-mode: nil -*-
+
+-module(flate_lz77).
+-export([decode/3]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-export([clone_output/3, decode_distance_pair/2]).
+-endif.
+
+distance_base() -> [
+  { 0,     1},
+  { 1,     2},
+  { 2,     3},
+  { 3,     4},
+  { 4,     5},
+  { 5,     7},
+  { 6,     9},
+  { 7,    13},
+  { 8,    17},
+  { 9,    25},
+  {10,    33},
+  {11,    49},
+  {12,    65},
+  {13,    97},
+  {14,   129},
+  {15,   193},
+  {16,   257},
+  {17,   385},
+  {18,   513},
+  {19,   769},
+  {20,  1025},
+  {21,  1537},
+  {22,  2049},
+  {23,  3073},
+  {24,  4097},
+  {25,  6145},
+  {26,  8193},
+  {27, 12289},
+  {28, 16385},
+  {29, 24577}
+].
+distance_base(Code) ->
+  {Code, Value} = lists:keyfind(Code, 1, distance_base()),
+  Value.
+
+distance_extra_bits() ->
+  % Code table for distance alphabet from RFC 1951 section 3.2.5
+  lists:concat([
+    [{X,  0} || X <- [ 0,  1, 2, 3]],
+    [{X,  1} || X <- [ 4,  5]],
+    [{X,  2} || X <- [ 6,  7]],
+    [{X,  3} || X <- [ 8,  9]],
+    [{X,  4} || X <- [10, 11]],
+    [{X,  5} || X <- [12, 13]],
+    [{X,  6} || X <- [14, 15]],
+    [{X,  7} || X <- [16, 17]],
+    [{X,  8} || X <- [18, 19]],
+    [{X,  9} || X <- [20, 21]],
+    [{X, 10} || X <- [22, 23]],
+    [{X, 11} || X <- [24, 25]],
+    [{X, 12} || X <- [26, 27]],
+    [{X, 13} || X <- [28, 29]]
+  ]).
+distance_extra_bits(Code) ->
+  lists:keyfind(Code, 1, distance_extra_bits()).
+
+distance_code_length(Code) ->
+  case Code of
+    257 -> {0,   3}; 258 -> {0,   4}; 259 -> {0,   5}; 260 -> {0,   6};
+    261 -> {0,   7}; 262 -> {0,   8}; 263 -> {0,   9}; 264 -> {0,  10};
+    265 -> {1,  11}; 266 -> {1,  13}; 267 -> {1,  15}; 268 -> {1,  17};
+    269 -> {2,  19}; 270 -> {2,  23}; 271 -> {2,  27}; 272 -> {2,  31};
+    273 -> {3,  35}; 274 -> {3,  43}; 275 -> {3,  51}; 276 -> {3,  59};
+    277 -> {4,  67}; 278 -> {4,  83}; 279 -> {4,  99}; 280 -> {4, 115};
+    281 -> {5, 131}; 282 -> {5, 163}; 283 -> {5, 195}; 284 -> {5, 227};
+    285 -> {0, 258}
+  end.
+
+decode_distance_pair(Code, Data) ->
+  case decode_distance_len(Code, Data) of
+    {more, N} -> {more, N};
+    {Length, LengthTail, BitsRead1} ->
+      case decode_distance(LengthTail) of
+        {more, N} -> {more, N};
+        {Distance, DistanceTail, BitsRead2} ->
+          {Length, Distance, DistanceTail, BitsRead1 + BitsRead2}
+      end
+  end.
+
+decode_distance_len(Code, Data) ->
+  {ExtraBits, Len} = distance_code_length(Code),
+  case flate_utils:read_bits(Data, ExtraBits) of
+    {error, insufficient_data} ->
+      {more, flate_utils:reverse_int(ExtraBits) div 8 + 1};
+    {Extra, Tail} ->
+      {Len + flate_utils:reverse_int(Extra, ExtraBits), Tail, ExtraBits}
+  end.
+
+decode_distance(Data) ->
+  case flate_utils:read_bits(Data, 5, []) of
+    {error, insufficient_data} -> {more, 1};
+    {RevCode, Extras} ->
+      Code = flate_utils:reverse_int(RevCode, 5),
+      Base = distance_base(Code),
+      {Code, ExtraBits} = distance_extra_bits(Code),
+      case flate_utils:read_bits(Extras, ExtraBits) of
+        {error, insufficient_data} -> {more, 1};
+        {Extra, Tail} ->
+          {Base + flate_utils:b2i(Extra) + 1, Tail, 5 + ExtraBits}
+      end
+  end.
+
+decode(Symbols, Code, Data) ->
+  case decode_distance_pair(Code, Data) of
+    {more, N} -> {more, N};
+    {Length, Dist, Tail, Read} ->
+      case clone_output(lists:flatten(Symbols), Dist, Length) of
+        {error, Reason} -> {error, Reason};
+        Output -> {ok, Output, Tail, Read}
+      end
+  end.
+
+clone_output(Symbols, Dist, _) when Dist > length(Symbols) ->
+  {error, {deflate_distance_too_far_back, Dist, length(Symbols)}};
+clone_output(Symbols, Dist, Length) ->
+  {_, Buf} = lists:split(length(Symbols)-Dist, Symbols),
+  clone_output(Buf, Length, [], []).
+
+clone_output(_, 0, Cur, Acc) ->
+  lists:concat(lists:reverse([lists:reverse(Cur)|Acc]));
+clone_output([], Len, Cur, Acc) ->
+  Buf = lists:concat(lists:reverse([lists:reverse(Cur)|Acc])),
+  clone_output(Buf, Len, [], [Buf]);
+clone_output([S|Buf], Length, Cur, Acc) ->
+  clone_output(Buf, Length-1, [S|Cur], Acc).
+
+-ifdef(TEST).
+
+clone_output_test_() -> [
+  ?_assertEqual("d",    clone_output("abcd", 1, 1)),
+  ?_assertEqual("dd",   clone_output("abcd", 1, 2)),
+  ?_assertEqual("dddd", clone_output("abcd", 1, 4)),
+
+  ?_assertEqual("c",    clone_output("abcd", 2, 1)),
+  ?_assertEqual("cd",   clone_output("abcd", 2, 2)),
+  ?_assertEqual("cdcd", clone_output("abcd", 2, 4)),
+
+  ?_assertEqual("abcd", clone_output("abcd", 4, 4)),
+  ?_assertEqual("abcda", clone_output("abcd", 4, 5)),
+  ?_assertEqual("abcdabcd", clone_output("abcd", 4, 8)),
+  ?_assertEqual("abcdabcda", clone_output("abcd", 4, 9)),
+
+  ?_assertEqual(lists:duplicate(258, $?), clone_output("?", 1, 258)),
+  ?_assertEqual(lists:flatten(lists:duplicate(129, "c?")),
+                clone_output("abc?", 2, 258))
+].
+
+-endif.
+
+-ifdef(DEAD_CODE).
+distance_length_base() -> [
+  { 0,   3}, { 1,   4}, { 2,   5}, { 3,   6}, { 4,   7}, { 5,   8}, { 6,   9},
+  { 7,  10}, { 8,  11}, { 9,  13}, {10,  15}, {11,  17}, {12,  19}, {13,  23},
+  {14,  27}, {15,  31}, {16,  35}, {17,  43}, {18,  51}, {19,  59}, {20,  67},
+  {21,  83}, {22,  99}, {23, 115}, {24, 131}, {25, 163}, {26, 195}, {27, 227},
+  {28, 258}
+].
+distance_length_base(Code) ->
+  {Code, Value} = lists:keyfind(Code, 1, distance_length_base()),
+  Value.
+
+-define(distance_code(Lower, Upper, Extra),
+        distance_code(N) when N >= Lower andalso N =< Upper div 4 ->
+	  {N, {5, Extra * 2 + 3}};
+        distance_code(N) when N > Upper div 4 andalso (N =< Upper) ->
+	  {N, {5, Extra * 2 + 3}}).
+
+distance_codes() -> [distance_code(N) || N <- lists:seq(1, 32768) ].
+distance_code(1) -> {1, {5, 0}};
+distance_code(2) -> {2, {5, 1}};
+distance_code(3) -> {3, {5, 2}};
+distance_code(4) -> {4, {5, 3}};
+?distance_code(      5,     8,   1);
+?distance_code(      9,    16,   2);
+?distance_code(     17,    32,   3);
+?distance_code(     33,    64,   4);
+?distance_code(     65,   128,   5);
+?distance_code(    129,   256,   6);
+?distance_code(    257,   512,   7);
+?distance_code(    513,  1024,   8);
+?distance_code(   1025,  2048,   9);
+?distance_code(   2049,  4096,  10);
+?distance_code(   4097,  8192,  11);
+?distance_code(   8193, 16384,  12);
+?distance_code(  16385, 32768,  13).
+-endif.
